e3fb85395c3ad383836b7c00a2bdb67eeebb48ad
ruudyaa <martarudnytska26@gmail.com>
2023-06-25 23:50:59 +0300

    Updates App file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,31 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-
-import { ToastContainer, toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+// toastify
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { toastifyOptions } from 'utils/toastifyOptions';
 
-import { GlobalStyle } from '../styles/GlobalStyle';
+// components
+import { GlobalStyle } from 'styles/GlobalStyle';
 import { Layout } from './Layout/Layout';
 import { Section } from './Section/Section';
 import { Title } from './Title/Title';
-
-import Filter from './Filter/Filter';
-
+import { Filter } from './Filter/Filter';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 
-import initialContacts from 'data/contacts.json';
-import useLocalStorage from 'hooks/useLocalStorage';
+// redux
+import { getContacts } from 'redux/contacts/contacts-selectors';
 
-export default function App() {
-  const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
-  const [filter, setFilter] = useState('');
-
-  const addContact = newContact => {
-    const isExist = contacts.some(
-      ({ name, number }) =>
-        name.toLowerCase().trim() === newContact.name.toLowerCase().trim() ||
-        number.trim() === newContact.number.trim()
-    );
-
-    if (isExist) {
-      return toast.error(
-        `${newContact.name}: is already in contacts`,
-        toastifyOptions
-      );
-    }
-
-    setContacts(contacts => [{ ...newContact, id: nanoid() }, ...contacts]);
-  };
-
-  const deleteContact = contactId => {
-    setContacts(contacts.filter(contact => contact.id !== contactId));
-  };
-
-  const changeFilter = e => {
-    setFilter(e.target.value.toLowerCase().trim());
-  };
-
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().trim().includes(normalizedFilter)
-    );
-
-    if (normalizedFilter && !filteredContacts.length) {
-      toast.warn(`No contacts matching your request`, toastifyOptions);
-    }
-
-    return filteredContacts;
-  };
-
+export const App = () => {
+  const contacts = useSelector(getContacts);
   return (
     <Layout>
-      <Section title="Phonebook">
-        <ContactForm onAddContact={addContact} />
-
+      <Section title="PhoneBook">
+        <ContactForm />
         {contacts.length > 0 && (
           <>
             <Title title="Contacts" />
-            <Filter value={filter} onChange={changeFilter} />
-            <ContactList
-              onDelete={deleteContact}
-              contacts={getVisibleContacts()}
-            />
+            <Filter />
+            <ContactList />
           </>
         )}
       </Section>
@@ -81,4 +33,4 @@ export default function App() {
       <GlobalStyle />
     </Layout>
   );
-}
+};
