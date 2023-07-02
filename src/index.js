2c519640eedd9c942bb252cce45b2140c079267c
ruudyaa <martarudnytska26@gmail.com>
2023-07-03 00:21:35 +0300

    Updates import.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
-import { App } from 'components/App';
+import App from 'components/App';
 
 import { theme } from 'styles/theme';
 
